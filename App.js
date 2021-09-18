b5c962d7924a6906e452de8d44e5c6585ad70208
LucaoMendes <lucas1999mendes@gmail.com>
2021-09-18 09:44:30 -0300

    Implementação do Drawer

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { NavigationContainer , useNavigation  } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { createDrawerNavigator } from '@react-navigation/drawer'
 //Pages
 import Inicial from './src/pages/Inicial';
 import Principal from './src/pages/Principal';
 import Produtos from './src/pages/Produtos';
 const Stack = createStackNavigator()
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Principal">
+const Drawer = createDrawerNavigator();
 
+function inicialScreenStack({navigation}){
+  return(
+    <Stack.Navigator initialRouteName="Inicial">
 
-        {/*Pagina de Login*/}
+        
         <Stack.Screen 
           name="Inicial" 
           component={Inicial} 
@@ -33,6 +33,117 @@ export default function App() {
             component={Produtos}/>
           
       </Stack.Navigator>
+  )
+}
+function principalScreenStack({navigation}){
+  return(
+    <Stack.Navigator initialRouteName="Principal">
+
+        
+        <Stack.Screen 
+          name="Inicial" 
+          component={Inicial} 
+          options={
+            {
+              headerShown: false
+            }
+          }
+          />
+
+          <Stack.Screen
+            name="Principal"
+            component={Principal}/>
+          
+          <Stack.Screen
+            name="Produtos"
+            component={Produtos}/>
+          
+      </Stack.Navigator>
+  )
+}
+function produtosScreenStack({navigation}){
+  return(
+    <Stack.Navigator initialRouteName="Produtos">
+
+        
+        <Stack.Screen 
+          name="Inicial" 
+          component={Inicial} 
+          options={
+            {
+              headerShown: false
+            }
+          }
+          />
+
+          <Stack.Screen
+            name="Principal"
+            component={Principal}/>
+          
+          <Stack.Screen
+            name="Produtos"
+            component={Produtos}/>
+          
+      </Stack.Navigator>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        
+        screenOptions={{
+          activeTintColor: '#e91e63',
+          itemStyle:{ marginVertical:5 }
+        }}>
+          <Drawer.Screen
+            name="inicial" 
+            component={inicialScreenStack} 
+            options={{ 
+              drawerLabel:'Login',
+              headerShown:false
+          }}
+          />
+          <Drawer.Screen
+            name="principal" 
+            component={principalScreenStack} 
+            options={{ 
+              drawerLabel:'Categorias',
+              headerShown:false}}
+          />
+          
+          
+        
+
+      </Drawer.Navigator>
     </NavigationContainer>
+    
+    /*
+    <NavigationContainer>
+       
+      <Stack.Navigator initialRouteName="Inicial">
+
+        
+        <Stack.Screen 
+          name="Inicial" 
+          component={Inicial} 
+          options={
+            {
+              headerShown: false
+            }
+          }
+          />
+
+          <Stack.Screen
+            name="Principal"
+            component={Principal}/>
+          
+          <Stack.Screen
+            name="Produtos"
+            component={Produtos}/>
+          
+      </Stack.Navigator>
+    </NavigationContainer> */
   );
 }
