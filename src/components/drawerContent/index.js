2d24fe299ad47ea6d413d38aea5a48a33891508b
LucaoMendes <lucas1999mendes@gmail.com>
2021-09-20 13:09:50 -0300

    Incorporação de novos botões no DRAWER

--- src/components/drawerContent/index.js
+++ src/components/drawerContent/index.js
@@ -2,19 +2,67 @@ import React, { Component } from "react";
 import { View } from "react-native";
 
 import styles from './style'
-import { AntDesign } from '@expo/vector-icons'; 
-import { DrawerContentScrollView , DrawerItem } from '@react-navigation/drawer'
+import { AntDesign , Octicons , MaterialIcons , FontAwesome5 ,Entypo } from '@expo/vector-icons'; 
+import { DrawerContentScrollView , DrawerItem, DrawerItemList } from '@react-navigation/drawer'
 
+function Hr(){
+    return(<View
+        style={styles.hr}
+      />)
+}
 export default function DrawerContent({navigation}){
         return(
             <DrawerContentScrollView >
                 <View style={styles.headerDrawerContainer}>
+
                 </View>
                 <DrawerItem 
-                label="OK" 
+                style={styles.drawerItem}
+                labelStyle={styles.drawerItemLabel}
+                label="Inicio" 
                 icon={()=> <AntDesign name="home" size={24} color="black" />}
                 onPress={()=>{navigation.navigate("Principal") }}
                     />
+                    <Hr/>
+                    <DrawerItem 
+                style={styles.drawerItem}
+                labelStyle={styles.drawerItemLabel}
+                label="Controle de Caixa" 
+                icon={()=> <FontAwesome5 name="cash-register" size={24} color="black" />}
+                onPress={()=>{ }}
+                    />
+                    <DrawerItem 
+                style={styles.drawerItem}
+                labelStyle={styles.drawerItemLabel}
+                label="Vendas" 
+                icon={()=> <Entypo name="price-tag" size={24} color="black" />}
+                onPress={()=>{ }}
+                    />
+                <DrawerItem 
+                style={styles.drawerItem}
+                labelStyle={styles.drawerItemLabel}
+                label="Cadastros" 
+                icon={()=> <FontAwesome5 name="pencil-ruler" size={24} color="black" />}
+                onPress={()=>{ }}
+                    />
+                     
+                    <Hr/>
+                    
+                    <DrawerItem 
+                style={styles.drawerItem}
+                labelStyle={styles.drawerItemLabel}
+                label="Estoque" 
+                icon={()=> <MaterialIcons name="inventory" size={24} color="black" />}
+                onPress={()=>{ }}
+                    />
+                    <DrawerItem 
+                style={styles.drawerItem}
+                labelStyle={styles.drawerItemLabel}
+                label="Relatorios" 
+                icon={()=> <Octicons name="graph" size={24} color="black" />}
+                onPress={()=>{ }}
+                    />
+                   
             </DrawerContentScrollView>
         )
     
--- src/components/drawerContent/style.js
+++ src/components/drawerContent/style.js
@@ -6,8 +6,21 @@ const styles = StyleSheet.create({
         height:200, 
         top:-5
     },
+    hr:{
+        borderBottomColor: 'black',
+        borderBottomWidth: 0.2,
+        width:"95%",
+        alignSelf:'center'
+      }
+    ,
     headerTitle:{
 
+    },
+    drawerItem:{
+
+    },
+    drawerItemLabel:{
+        fontSize:15
     }
 })
 export default styles
