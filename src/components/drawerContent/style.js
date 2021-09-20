cfa2974dca1cb4a7b08ae9a4902f439e277744b8
LucaoMendes <lucas1999mendes@gmail.com>
2021-09-20 15:07:13 -0300

    Implementação final dos drawerItems

--- src/components/drawerContent/style.js
+++ src/components/drawerContent/style.js
@@ -2,17 +2,21 @@ import { StyleSheet } from "react-native";
 import colors from '../../styles/colors'
 const styles = StyleSheet.create({
     headerDrawerContainer:{
+        flex:1,
+        flexDirection:"column",
         backgroundColor: colors.backgroundDrawerHeaderContainer,
-        height:200, 
-        top:-5
+        height:220, 
+        top:-5,
+        justifyContent:'center',
+        alignItems:'center',
+        width:'100%'
     },
     hr:{
         borderBottomColor: 'black',
         borderBottomWidth: 0.2,
         width:"95%",
         alignSelf:'center'
-      }
-    ,
+    },
     headerTitle:{
 
     },
@@ -21,6 +25,28 @@ const styles = StyleSheet.create({
     },
     drawerItemLabel:{
         fontSize:15
+    },
+    avatarLoja:{
+        marginBottom:5
+    },
+    drawerConfigPerfil:{
+        left:20,
+        minWidth:'100%',
+        bottom:-15,
+    },
+    drawerConfigPerfilLabel:{
+        
+        color:colors.fontColorPrimaryHeaderContainer
+    },
+    drawerAuthenticationLevel:{
+        width:'100%',
+        left:20,
+        color:colors.fontColorPrimaryHeaderContainer
+    },
+    drawerCompanyName:{
+        width:'100%',
+        textAlign:'center',
+        color:colors.fontColorPrimaryHeaderContainer
     }
 })
 export default styles
